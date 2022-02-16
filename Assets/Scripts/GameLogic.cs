3807529883bcf2849a4af57529c3ba2bee4149dd
Xiunan <xiunany@andrew.cmu.edu>
2022-02-15 21:54:56 -0500

    upload aireconsole plugin

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class GameLogic : MonoBehaviour
+{
+    private void Awake()
+    {
+        AirConsole.instance.onMessage += OnMessage;
+    }
+
+    void OnMessage(int fromDeviceID, JToken data)
+    {
+        Debug.Log("message from " + fromDeviceID + ", data: " + data);
+        if(data["action"]!= null && data["action"].ToString().Equals ("interact"))
+        {
+            Camera.main.backgroundColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+    }
+
+    private void OnDestroy()
+    {
+       if(AirConsole.instance != null)
+        {
+            AirConsole.instance.onMessage -= OnMessage;
+        }
+    }
+}
