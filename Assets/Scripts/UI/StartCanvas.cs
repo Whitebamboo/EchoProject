aa74970815bb2dc12108ebc66e9d188ab1942a0a
Xiunan <xiunany@andrew.cmu.edu>
2022-04-15 13:04:05 -0400

    update joystick visible, consumer collider

--- Assets/Scripts/UI/StartCanvas.cs
+++ Assets/Scripts/UI/StartCanvas.cs
@@ -152,7 +152,7 @@ public class StartCanvas : UIScreenBase
 
         if (currIndex >= pages.Length)
         {
-            GameManager.instance.SwitchState(GameState.Tutorial);
+            GameManager.instance.SwitchState(GameState.Fantasy);
             CloseScreen();
             return;
         }
